0f7296dbcbad19162cc3f5bd0535ae3e3297b4a2
Erlis <erliserlis6@gmail.com>
2021-02-15 19:45:15 +0100

    Initial Commit

---
+++ Flixter - Assigment 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixter - Assigment 1
+//
+//  Created by Erlis Ibraimi on 15.2.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
